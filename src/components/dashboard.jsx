7739c55373557a6a2dd66dfe22954826fc7d2be1
priscy-d <82945778+priscy-d@users.noreply.github.com>
2023-05-30 17:51:34 +0100

    dashboard layout done

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,14 +1,85 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Form } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
-    <Container className="mt-5">
-      <h2>Dashboard</h2>
+    <Container className="main mt-5">
       <Row>
+        <Col md={8}>
+          <h2 className="mb-3">Dashboard</h2>
+        </Col>
         <Col>
-          <p>Receivables</p>
-          <p>Total unpaid invoices</p>
+          <Form.Group>
+            <div className="d-flex flex-row">
+              <Form.Control
+                className="date me-2"
+                type="date"
+                placeholder="start date"
+                size="sm"
+                id="date"
+              />
+
+              <Form.Control
+                className="date"
+                type="date"
+                placeholder="end date"
+                size="sm"
+                id="date"
+              />
+            </div>
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row className="my-3">
+        <Col className="mt-2">
+          <h5>Receivables</h5>
+          <p>Amount yet to be paid by customers</p>
+          <hr />
+          <p>
+            Total unpaid invoices:<b>¢2400</b>
+          </p>
+        </Col>
+        <Col className="mt-2">
+          <h5>Paid</h5>
+          <p> Total amount paid by customers</p>
+          <hr />
+          <p>
+            Total paid invoices:<b>¢2400</b>
+          </p>
+        </Col>
+      </Row>
+      <Row className="my-3">
+        <Col className="mt-2">
+          <h5>Cash flow chart</h5>
+          <p> Total amount made and due customers</p>
+          <hr />
+          <Row>
+            <Col sm={2}>
+              <p>income</p>
+            </Col>
+            <Col sm={2}>
+              <p>unpaid</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="my-3">
+        <Col className="mt-2">
+          <h5>Income by type</h5>
+          <p>Total income across invoice types </p>
+          <hr />
+        </Col>
+        <Col className="mt-2">
+          <h5>Account balance</h5>
+          <p> Total amount in bank account</p>
+          <hr />
+          <Row>
+            <Col md={10}>Cash</Col>
+            <Col md={2}>
+              <b>¢2400</b>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Col, Container, Row, Stack } from "react-bootstrap";
+import { Col, Row, Stack } from "react-bootstrap";
 import { BiUserCircle, BiNotification, BiSearchAlt2 } from "react-icons/bi";
 import { AiOutlineSetting, AiOutlineDashboard } from "react-icons/ai";
 import { FaFileInvoice } from "react-icons/fa";
@@ -8,7 +8,7 @@ import { BsArchive, BsFillPersonPlusFill } from "react-icons/bs";
 
 function Sidebar() {
   return (
-    <Container className="sidebar ">
+    <div className="sidebar ">
       <Row>
         <Col xs={2} className="mini-section pt-5 px-2">
           <Stack gap={4}>
@@ -20,7 +20,9 @@ function Sidebar() {
         </Col>
         <Col className="large-section pt-5 px-2">
           <Stack gap={3}>
-            <h4>User</h4>
+            <h5>
+              Hi, <b>User</b>
+            </h5>
             <Link to="dashboard" className="nav-items mx-2 ">
               <AiOutlineDashboard /> <p className="mx-2 text">Dashboard</p>
             </Link>
@@ -36,7 +38,7 @@ function Sidebar() {
           </Stack>
         </Col>
       </Row>
-    </Container>
+    </div>
   );
 }
 
