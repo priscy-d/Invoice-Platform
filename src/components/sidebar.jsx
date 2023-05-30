8d5aab742a243a5ad5afef6bd400067e16846bfb
priscy-d <82945778+priscy-d@users.noreply.github.com>
2023-05-30 15:23:56 +0100

    sidebar commenced

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Col, Container, Row, Stack } from "react-bootstrap";
+import { BiUserCircle, BiNotification, BiSearchAlt2 } from "react-icons/bi";
+import { AiOutlineSetting, AiOutlineDashboard } from "react-icons/ai";
+import { FaFileInvoice } from "react-icons/fa";
+import { BsArchive } from "react-icons/bs";
 
 function Sidebar() {
-  const navigate = useNavigate();
-
   return (
-    <div className="sidebar">
-      <h2>sidebar goes here</h2>
-      <button onClick={() => navigate("dashboard")}> Dashboard</button>
-      <button onClick={() => navigate("invoices")}>Invoices</button>
-      <button onClick={() => navigate("items")}>Item</button>
-    </div>
+    <Container className="sidebar ">
+      <Row>
+        <Col xs={2} className="mini-section pt-5 px-2">
+          <Stack gap={4}>
+            <BiUserCircle className="icon" />
+            <BiSearchAlt2 className="icon" />
+            <BiNotification className="icon" />
+            <AiOutlineSetting className="icon" />
+          </Stack>
+        </Col>
+        <Col className="large-section pt-5 px-2">
+          <Stack gap={3}>
+            <h4>User</h4>
+            <Link to="dashboard" className="nav-items  ">
+              <AiOutlineDashboard /> <p className="mx-2">Dashboard</p>
+            </Link>
+            <Link to="invoices" className="nav-items   ">
+              <FaFileInvoice /> <p className="mx-2">Invoice</p>
+            </Link>
+            <Link to="items" className="nav-items  ">
+              <BsArchive /> <p className="mx-2">Items</p>
+            </Link>
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
